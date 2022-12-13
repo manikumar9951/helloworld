d0dea044b07ce148f33bd2a95a4bc84ccdcd6896
Padmanabham NVMKumar <Padmanabham.NVMKumar@lntinfotech.com>
2022-12-13 17:08:18 +0530

    added files

--- src/main/java/com/example/docker/demo/HelloWorldController.java
+++ src/main/java/com/example/docker/demo/HelloWorldController.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	@GetMapping("/get")
-	Public string sayHello(){
+	Public String sayHello(){
 		return "Hello Spring Boot docker";
 	}
 
